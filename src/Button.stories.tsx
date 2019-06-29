96455364ac6b98646fc5da61958cdf5561929e79
Thanh Nguyen <vnscriptkid@gmail.com>
2019-06-29 15:46:50 +0700

    Config Eslint and Prettier with Typescript

--- src/Button.stories.tsx
+++ src/Button.stories.tsx
@@ -1,15 +1,15 @@
 import { storiesOf } from "@storybook/react";
 import { Button } from "./Button";
-import React from 'react';
+import React, { ReactElement } from 'react';
 // import { text, boolean } from "@storybook/addon-knobs/react";
 
 storiesOf('Button', module)
-  .add('with text', () => (
-    <Button>Hi</Button>
-  ))
-  .add('with emoji', () => (
-    <Button><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>
-  ))
-  .add('with label', () => (
-      <Button label="Hello"/>
-  ))
+    .add('with text', (): ReactElement => (
+        <Button label="hi" />
+    ))
+    .add('with emoji', (): ReactElement => (
+        <Button label="good"><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>
+    ))
+    .add('with label', (): ReactElement => (
+        <Button label="Hello" />
+    ))
